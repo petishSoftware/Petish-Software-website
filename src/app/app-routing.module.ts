c0366941bd6026db4f544d02dda9aad87100ba73
shaphankirui <79963010+shaphankirui@users.noreply.github.com>
2024-03-13 17:20:03 +0200

    routings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeOneComponent } from './components/pages/home-one/home-one.component';
-import { HomeTwoComponent } from './components/pages/home-two/home-two.component';
-import { HomeThreeComponent } from './components/pages/home-three/home-three.component';
 import { AboutComponent } from './components/pages/about/about.component';
 import { SolutionsComponent } from './components/pages/solutions/solutions.component';
 import { SolutionsDetailsComponent } from './components/pages/solutions-details/solutions-details.component';
@@ -17,15 +15,13 @@ import { TestimonialsComponent } from './components/pages/testimonials/testimoni
 import { SignUpComponent } from './components/pages/sign-up/sign-up.component';
 import { SignInComponent } from './components/pages/sign-in/sign-in.component';
 import { ErrorComponent } from './components/pages/error/error.component';
-import { FaqComponent } from './components/pages/faq/faq.component';
 import { TermsConditionsComponent } from './components/pages/terms-conditions/terms-conditions.component';
 import { PrivacyPolicyComponent } from './components/pages/privacy-policy/privacy-policy.component';
 import { ContactComponent } from './components/pages/contact/contact.component';
 
 const routes: Routes = [
     {path: '', component: HomeOneComponent},
-    {path: 'home-two', component: HomeTwoComponent},
-    {path: 'home-three', component: HomeThreeComponent},
+
     {path: 'about', component: AboutComponent},
     {path: 'solutions', component: SolutionsComponent},
     {path: 'solutions-details', component: SolutionsDetailsComponent},
@@ -40,7 +36,6 @@ const routes: Routes = [
     {path: 'sign-up', component: SignUpComponent},
     {path: 'sign-in', component: SignInComponent},
     {path: 'error', component: ErrorComponent},
-    {path: 'faq', component: FaqComponent},
     {path: 'terms-condition', component: TermsConditionsComponent},
     {path: 'privacy-policy', component: PrivacyPolicyComponent},
     {path: 'contact', component: ContactComponent},
